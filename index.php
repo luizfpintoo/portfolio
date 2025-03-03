b6340d62920ab87007c74be025ea45363d0881c0
luizfpintoo <luizfspintoo@outlook.com>
2025-03-03 13:24:35 -0300

    Conhecendo funções

--- index.php
+++ index.php
@@ -28,34 +28,74 @@
             "descricao" => "Meu primeiro projeto escrito em PHP e HTML"
         ],
         [
-            "titulo" => "Projeto 2",
+            "titulo" => "Sistema de Gerenciamento de Tarefas",
             "ano" => 2025,
-            "data" => "2025-03-02",
+            "data" => "2025-03-03",
             "finalizado" => false,
-            "descricao" => "Meu primeiro projeto escrito em PHP e HTML"
+            "descricao" => "Um sistema para criar, editar e acompanhar tarefas usando PHP e MySQL"
         ],
         [
-            "titulo" => "Projeto 3",
+            "titulo" => "E-commerce Simples",
             "ano" => 2025,
-            "data" => "2025-03-02",
+            "data" => "2025-03-04",
             "finalizado" => false,
-            "descricao" => "Meu primeiro projeto escrito em PHP e HTML"
+            "descricao" => "Uma loja virtual básica com carrinho de compras e integração com API de pagamento"
+        ],
+        [
+            "titulo" => "Chat em Tempo Real",
+            "ano" => 2025,
+            "data" => "2025-03-05",
+            "finalizado" => false,
+            "descricao" => "Um chat online utilizando PHP, JavaScript e WebSocket para mensagens instantâneas"
+        ],
+        [
+            "titulo" => "Blog com Painel Administrativo",
+            "ano" => 2025,
+            "data" => "2025-03-06",
+            "finalizado" => false,
+            "descricao" => "Um blog com interface de postagem e painel para o administrador"
+        ],
+        [
+            "titulo" => "Sistema de Controle de Estoque",
+            "ano" => 2025,
+            "data" => "2025-03-07",
+            "finalizado" => true,
+            "descricao" => "Aplicativo que gerencia entradas e saídas de produtos com relatórios detalhados"
         ]
     ];
+
 
     function verificarFinalizado($projeto)
     {
         if ($projeto["finalizado"]) {
             return '<span class="green">Finalizado ✅</span>';
-        }else {
+        } else {
             return '<span class="red">Não finalizado ⛔</span>';
         }
+    }
+
+    function filtrarProjetos($projetos, $finalizado = null)
+    {
+
+        if (is_null($finalizado)) {
+            return $projetos;
+        }
+
+        $filtrados = [];
+
+        foreach ($projetos as $projeto) {
+            if (! is_null($finalizado) && $projeto["finalizado"]) {
+                $filtrados[] = $projeto;
+            }
+        }
+
+        return $filtrados;
     }
 
 
     ?>
 
-    <?php foreach ($projetos as $projeto): ?>
+    <?php foreach (filtrarProjetos($projetos, true) as $projeto): ?>
         <div
             <?php if (($projeto["ano"] - 1994) > 30) : ?>
             style="background-color:rgb(141, 208, 234);"
